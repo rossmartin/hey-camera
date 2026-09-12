dc92f49ad3ff67ede95583ea1774ae15683b7cb4
Ross Martin <2498502+rossmartin@users.noreply.github.com>
2026-09-12 09:59:12 -0500

    Update support email

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -8,5 +8,6 @@ export function withBase(path: string): string {
   return `${base}${suffix}` || '/';
 }
 
-export const APP_STORE_URL = 'https://apps.apple.com/us/app/hey-camera/id1271273970';
-export const SUPPORT_EMAIL = 'heycamera.app@gmail.com';
+export const APP_STORE_URL =
+  'https://apps.apple.com/us/app/hey-camera/id1271273970';
+export const SUPPORT_EMAIL = 'support@heycamera.app';
