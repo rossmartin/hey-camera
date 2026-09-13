e8a0b02b761791eb4a1214503a0ff35753057e86
Ross Martin <2498502+rossmartin@users.noreply.github.com>
2026-09-13 08:58:08 -0500

    SERP improvements

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -8,6 +8,7 @@ export function withBase(path: string): string {
   return `${base}${suffix}` || '/';
 }
 
-export const APP_STORE_URL =
-  'https://apps.apple.com/us/app/hey-camera/id1271273970';
+export const APP_STORE_ID = '1271273970';
+
+export const APP_STORE_URL = `https://apps.apple.com/us/app/hey-camera/id${APP_STORE_ID}`;
 export const SUPPORT_EMAIL = 'support@heycamera.app';
